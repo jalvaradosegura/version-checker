c4e37cd5e3539bfbd2babcc87f6f32da4cf2c4ed
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-01-10 17:39:13 -0300

    Try to grab the version from .toml if not provided
    If the entry point is executed without a version parameter, the code
    will try to get it from the pyproject.toml. If it doesn't find it, it
    will raise an exception.

--- tests/test_version_checker.py
+++ tests/test_version_checker.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from version_checker.main import main, version_in_file
+import pytest
+
+from version_checker.main import main, version_in_file, get_version_from_toml
 from .utils import write_something_to_file
 
 
@@ -24,3 +26,23 @@ def test_cmd_version_in_file(tmp_file_path: Path):
     write_something_to_file(tmp_file_path, "version is in the file, 2.0.0")
     response = main(["--version", "2.0.0", "--files", str(tmp_file_path)])
     assert response == 0
+
+
+def test_cmd_no_version_provided_as_parameter(tmp_file_path: Path):
+    write_something_to_file(tmp_file_path, "I don't have any version")
+    response = main(["--files", str(tmp_file_path)])
+    assert response == 1
+
+
+def test_get_version_from_toml(tmp_file_path: Path):
+    write_something_to_file(
+        tmp_file_path, 'name = "version-checker"\nversion = "0.1.0"'
+    )
+    response = get_version_from_toml(tmp_file_path)
+    assert response == "0.1.0"
+
+
+def test_get_version_from_toml_with_no_version_defined(tmp_file_path: Path):
+    write_something_to_file(tmp_file_path, 'name = "version-checker"\n')
+    with pytest.raises(ValueError):
+        get_version_from_toml(tmp_file_path)
--- version_checker/main.py
+++ version_checker/main.py
@@ -3,6 +3,18 @@ import typing
 from pathlib import Path
 
 
+def get_version_from_toml(toml_file_path: typing.Union[Path]) -> str:
+    with open(toml_file_path, "r") as toml_file:
+        for line in toml_file.readlines():
+            if line.startswith("version = "):
+                version = line.split("=")[1].strip().replace('"', "")
+                return version
+        raise ValueError(
+            "Couldn't find package version within the .toml file.\n"
+            "Try giving a value to the --version parameter."
+        )
+
+
 def version_in_file(version: str, file_path: typing.Union[str, Path]) -> bool:
     with open(file_path, "r") as file:
         content = file.read()
@@ -16,6 +28,10 @@ def main(argv: typing.Optional[typing.List[str]] = None):
         "--files", nargs="*", help="files in which the version will be checked"
     )
     args = parser.parse_args(argv)
+
+    if args.version is None:
+        args.version = get_version_from_toml("pyproject.toml")
+
     return (
         0
         if all(version_in_file(args.version, file) for file in args.files)
