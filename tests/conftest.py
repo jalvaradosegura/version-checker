bc70ece33df89a8bc1c02a4dfacb49758778e3df
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-01-11 00:51:37 -0300

    Add hooks for the repository

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 
 import pytest
-from _pytest.logging import caplog as _caplog
+from _pytest.logging import caplog as _caplog  # noqa F401
 from loguru import logger
 
 
@@ -13,7 +13,7 @@ def tmp_file_path(tmp_path: Path) -> Path:
 
 
 @pytest.fixture
-def caplog(_caplog):
+def caplog(_caplog):  # noqa F811
     class PropagateHandler(logging.Handler):
         def emit(self, record):
             logging.getLogger(record.name).handle(record)
--- tests/test_version_checker.py
+++ tests/test_version_checker.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import pytest
 
 from version_checker.main import (
+    check_files,
+    get_version_from_toml,
     main,
     version_in_file,
-    get_version_from_toml,
-    check_files,
 )
+
 from .utils import write_something_to_file
 
 
@@ -68,9 +69,7 @@ def test_cmd_2_files_provided_one_does_not_contain_the_version(
     assert response == 1
 
 
-def test_cmd_2_files_provided_both_contain_the_version(
-    tmp_file_path: Path,
-):
+def test_cmd_2_files_provided_both_contain_the_version(tmp_file_path: Path,):
     write_something_to_file(tmp_file_path, "I have the version: 2.0.0")
     another_file = tmp_file_path.parent / "another_file.txt"
     write_something_to_file(another_file, "I have it too: 2.0.0")
